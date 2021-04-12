ca04c874e4203ad72cd34b2e76b219fe512d726e
Pedro Borges <pedro.hov.borges@gmail.com>
2021-04-12 20:29:59 -0300

    add responsive offset

--- src/components/cCol/cCol.vue
+++ src/components/cCol/cCol.vue
@@ -3,6 +3,7 @@
         :class="[
             'c-col',
             `c-col--col-${cols}`,
+            `c-col--offset-${offset}`,
 
             // responsive | {} with ` doesn't work
             xs > 0 ? `c-col--col-xs-${xs}` : '',
@@ -10,6 +11,12 @@
             md > 0 ? `c-col--col-md-${md}` : '',
             lg > 0 ? `c-col--col-lg-${lg}` : '',
             xl > 0 ? `c-col--col-xl-${xl}` : '',
+            // offset
+            offsetXs > 0 ? `c-col--offset-xs-${offsetXs}` : '',
+            offsetSm > 0 ? `c-col--offset-sm-${offsetSm}` : '',
+            offsetMd > 0 ? `c-col--offset-md-${offsetMd}` : '',
+            offsetLg > 0 ? `c-col--offset-lg-${offsetLg}` : '',
+            offsetXl > 0 ? `c-col--offset-xl-${offsetXl}` : '',
         ]"
     >
         <slot></slot>
@@ -24,11 +31,20 @@
     export default class cCol extends Vue {
         static install: (vue: any) => void;
 
+        // cols
         @Prop({ type: [String, Number], default: "12" }) public cols!: string | number;
         @Prop({ type: [String, Number], default: "0" }) public xs!: string | number;
         @Prop({ type: [String, Number], default: "0" }) public sm!: string | number;
         @Prop({ type: [String, Number], default: "0" }) public md!: string | number;
         @Prop({ type: [String, Number], default: "0" }) public lg!: string | number;
         @Prop({ type: [String, Number], default: "0" }) public xl!: string | number;
+
+        // offset
+        @Prop({ type: [String, Number], default: "0" }) public offset!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public offsetXs!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public offsetSm!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public offsetMd!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public offsetLg!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public offsetXl!: string | number;
     }
 </script>
