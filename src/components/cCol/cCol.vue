815000dbf75a1ef4efa8a905ce7de61d43a67b1a
Pedro Borges <pedro.hov.borges@gmail.com>
2021-04-12 20:17:29 -0300

    add responsive props

--- src/components/cCol/cCol.vue
+++ src/components/cCol/cCol.vue
@@ -2,7 +2,14 @@
     <div
         :class="[
             'c-col',
-            `c-col--cols-${this.cols}`
+            `c-col--col-${cols}`,
+
+            // responsive | {} with ` doesn't work
+            xs > 0 ? `c-col--col-xs-${xs}` : '',
+            sm > 0 ? `c-col--col-sm-${sm}` : '',
+            md > 0 ? `c-col--col-md-${md}` : '',
+            lg > 0 ? `c-col--col-lg-${lg}` : '',
+            xl > 0 ? `c-col--col-xl-${xl}` : '',
         ]"
     >
         <slot></slot>
@@ -18,5 +25,10 @@
         static install: (vue: any) => void;
 
         @Prop({ type: [String, Number], default: "12" }) public cols!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public xs!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public sm!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public md!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public lg!: string | number;
+        @Prop({ type: [String, Number], default: "0" }) public xl!: string | number;
     }
 </script>
