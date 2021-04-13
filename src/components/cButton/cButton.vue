d44e741793b37f00087d8c9f427e89c1f3501633
Pedro Borges <pedro.hov.borges@gmail.com>
2021-04-12 21:28:52 -0300

    add button types and colors

--- src/components/cButton/cButton.vue
+++ src/components/cButton/cButton.vue
@@ -2,17 +2,29 @@
     <button
         :class="[
             'c-button',
-            { 'c-button--active': !!this.active },
+            { 'c-button--active': !!active },
 
-            { 'c-button--primary': !this.color && !this.danger && !this.success && !this.warning && !this.dark && !this.light },
-            { 'c-button--success': !!this.success },
-            { 'c-button--warning': !!this.warning },
-            { 'c-button--danger': !!this.danger },
-            { 'c-button--dark': !!this.dark },
-            { 'c-button--light': !!this.light }
+            // colors
+            { 'c-button--primary': !color && !danger && !success && !warning && !dark && !light },
+            { 'c-button--success': !!success },
+            { 'c-button--warning': !!warning },
+            { 'c-button--danger': !!danger },
+            { 'c-button--dark': !!dark },
+            { 'c-button--light': !!light },
+
+            // types
+            { 'c-button--default': !plain && !flat && !border && !gradient && !raised && !text && !shadow && !floating },
+            { 'c-button--plain' : !!plain },
+            { 'c-button--flat' : !!flat },
+            { 'c-button--border' : !!border },
+            { 'c-button--gradient' : !!gradient },
+            { 'c-button--raised' : !!raised },
+            { 'c-button--text' : !!text },
+            { 'c-button--shadow' : !!shadow },
+            { 'c-button--floating' : !!floating },
         ]"
         :style="{
-            '--c-color': this.$data.__color
+            '--c-color': $data.__color
         }"
     >
         <div
@@ -32,10 +44,17 @@
     @Component
     export default class cButton extends cComponent {
 
+        @Prop({ type: Boolean, default: false }) public plain!: boolean;
+        @Prop({ type: Boolean, default: false }) public flat!: boolean;
+        @Prop({ type: Boolean, default: false }) public border!: boolean;
+        @Prop({ type: Boolean, default: false }) public gradient!: boolean;
+        @Prop({ type: Boolean, default: false }) public raised!: boolean;
+        @Prop({ type: Boolean, default: false }) public text!: boolean;
+        @Prop({ type: Boolean, default: false }) public shadow!: boolean;
+        @Prop({ type: Boolean, default: false }) public floating!: boolean;
+
         mounted() {
             console.log("cButton");
         }
     }
 </script>
-
-<style></style>
--- src/components/cComponent/cComponent.vue
+++ src/components/cComponent/cComponent.vue
@@ -10,18 +10,18 @@
 
     @Component
     export default class cComponent extends Vue {
-        static install: (vue: any) => void
+        static install: (vue: any) => void;
 
-        __color: any = null
+        __color: any = null;
 
-        @Prop({ type: String, default: null }) color!: string
-        @Prop({ type: Boolean, default: false }) primary!: boolean
-        @Prop({ type: Boolean, default: false }) danger!: boolean
-        @Prop({ type: Boolean, default: false }) success!: boolean
-        @Prop({ type: Boolean, default: false }) warning!: boolean
-        @Prop({ type: Boolean, default: false }) dark!: boolean
-        @Prop({ type: Boolean, default: false }) light!: boolean
-        @Prop({ type: Boolean, default: false }) active!: boolean
+        @Prop({ type: String, default: null }) color!: string;
+        @Prop({ type: Boolean, default: false }) primary!: boolean;
+        @Prop({ type: Boolean, default: false }) danger!: boolean;
+        @Prop({ type: Boolean, default: false }) success!: boolean;
+        @Prop({ type: Boolean, default: false }) warning!: boolean;
+        @Prop({ type: Boolean, default: false }) dark!: boolean;
+        @Prop({ type: Boolean, default: false }) light!: boolean;
+        @Prop({ type: Boolean, default: false }) active!: boolean;
 
         mounted() {
             this.$data.__color = this.color ? getColor(this.color) : '';
